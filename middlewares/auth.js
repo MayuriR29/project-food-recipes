06c5dc3705af0b243f5e0601c42139f1af49860a
ngyl88 <ng_yl88@hotmail.com>
2018-07-23 16:27:45 +0800

    update auth to use findById

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,12 +2,8 @@ const Recipe = require("../models/recipe");
 
 const rejectRequestIfIdsDontMatch = async (req, res, next) => {
   //todo: find better way to find recipe by id
-  const requestedRecipe = await Recipe.find({
-    _id: req.params.recipeId
-  });
-console.log('in auth user_id',requestedRecipe);
-console.log('requestedRecipe[0].contributorId',requestedRecipe[0].contributorId);
-  if (requestedRecipe[0].contributorId == req.user.id) {
+  const requestedRecipe = await Recipe.findById(req.params.recipeId);
+  if (requestedRecipe.contributorId == req.user.id) {
     next();
   } else {
     res.status(401).json({ message: "unauthorized" });
