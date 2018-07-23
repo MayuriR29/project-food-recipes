1b69dc9f1cb56a611352cf097b74506a1e82acbe
ngyl88 <ng_yl88@hotmail.com>
2018-07-23 16:09:37 +0800

    created .env, move JWT secret

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const passport = require("passport");
 const passportJWT = require("passport-jwt");
 const ExtractJwt = passportJWT.ExtractJwt;
@@ -7,7 +9,7 @@ const User = require("../models/user");
 
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: "some_secret"
+  secretOrKey: process.env.JWT_SECRET
 };
 
 const jwtStrategy = new JwtStrategy(jwtOptions, async (jwt_payload, done) => {
