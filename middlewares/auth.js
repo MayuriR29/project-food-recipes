9083eec26d63ecf78427c18aa5f34766b49c7351
ngyl88 <ng_yl88@hotmail.com>
2018-07-23 16:29:58 +0800

    remove unnecessary comments

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,6 @@
 const Recipe = require("../models/recipe");
 
 const rejectRequestIfIdsDontMatch = async (req, res, next) => {
-  //todo: find better way to find recipe by id
   const requestedRecipe = await Recipe.findById(req.params.recipeId);
   if (requestedRecipe.contributorId == req.user.id) {
     next();
