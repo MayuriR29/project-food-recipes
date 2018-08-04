40de0f9a939f5f78bace06552a6211763cbd8661
MayuriR29 <mayuri.gr@gmail.com>
2018-08-04 15:31:26 +0800

    refactored code using wrapper function for try catch

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,32 +1,66 @@
 const express = require("express");
-const router = express.Router();
-
-const recipeService = require('../middlewares/recipeService');
+const Recipe = require("../models/recipe");
 
 const { passport } = require("../config/passport");
 const validationErr = require("../middlewares/mongooseErrorMiddleware");
 const { rejectRequestIfIdsDontMatch } = require("../middlewares/auth");
+
+const asyncFunctionErrorWrapper = require("../utils/asyncFunctionErrorWrapper");
+
+const router = express.Router();
 router.use(express.json());
 
 //GET recipe listing
-router.get("/", recipeService.returnRecipesWithContributor);
+router.get(
+  "/",
+  asyncFunctionErrorWrapper(async (req, res, next) => {
+    const recipes = await Recipe.find().populate("contributorId");
+    res.json(recipes).status(200);
+  })
+);
 
-//POST recipes
-router.post("/", recipeService.addRecipe);
+router.post(
+  "/",
+  asyncFunctionErrorWrapper(async (req, res, next) => {
+    const newRecipe = new Recipe({
+      title: req.body.title,
+      contributorId: req.body.contributorId,
+      comments: req.body.comments,
+      ingredients: req.body.ingredients
+    });
+    await newRecipe.save();
+    res.status(201).json({ message: "Recipe added successfully" });
+  })
+);
 
-//PUT recipes
 router.put(
   "/:recipeId",
   passport.authenticate("jwt", { session: false }),
   rejectRequestIfIdsDontMatch,
-  recipeService.updateRecipe
+  asyncFunctionErrorWrapper(async (req, res, next) => {
+    try {
+      await Recipe.findByIdAndUpdate(req.params.recipeId, req.body);
+      res.status(204).json();
+    } catch (err) {
+      console.error("Error in PUT recipes", err);
+      next(err);
+    }
+  })
 );
 //DELETE recipes
 router.delete(
   "/:recipeId",
   passport.authenticate("jwt", { session: false }),
   rejectRequestIfIdsDontMatch,
-  recipeService.deleteRecipe
+  asyncFunctionErrorWrapper(async (req, res, next) => {
+    try {
+      await Recipe.findByIdAndDelete(req.params.recipeId, res.body);
+      res.status(204).json();
+    } catch (err) {
+      console.err("Error in DELETE recipes", err);
+      next(err);
+    }
+  })
 );
 
 module.exports = app => {
